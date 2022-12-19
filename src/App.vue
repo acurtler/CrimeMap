4766ff3ce73b0fef7d81700321f933e6b7cb9805
Anna Curtler <curt8897@stthomas.edu>
2022-12-18 19:23:01 -0600

    marker

--- src/App.vue
+++ src/App.vue
@@ -550,8 +550,16 @@ export default {
                 var myIcon = L.icon({
                     markerColor: 'red'
                 });
-                var marker = L.marker([data[0].lat, data[0].lon],{}).addTo(this.leaflet.map);
-                /*addPopups(data[0].lat, data[0].lon, event.date, event.time, event.incident);*/
+                var greenIcon = new L.Icon({
+                iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+                shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+                iconSize: [25, 41],
+                iconAnchor: [12, 41],
+                popupAnchor: [1, -34],
+                shadowSize: [41, 41]
+                });
+                var marker = L.marker([data[0].lat, data[0].lon],{icon: greenIcon}).addTo(this.leaflet.map);
+                marker.bindPopup(event.incident + "<br>" + "Date of incident: " + event.date + "<br>" + "Time of incident: " + event.time);
             }).catch((error) => {
                     console.log(error);
             });
